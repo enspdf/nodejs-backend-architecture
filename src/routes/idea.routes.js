626a59d27de24afd3d8c0ebdd8cebf09c41feedd
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-06-29 20:59:23 -0500

    ADD swagger UI configuration

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -1,17 +1,17 @@
 const { Router } = require("express");
-const { ParseInteMiddleware } = require("../middlewares");
+const { ParseInteMiddleware, AuthMiddleware } = require("../middlewares");
 
 module.exports = function ({ IdeaController }) {
   const router = Router();
 
-  router.get("/", ParseInteMiddleware, IdeaController.getAll);
+  router.get("/", [ParseInteMiddleware], IdeaController.getAll);
   router.get("/:ideaId", IdeaController.get);
   router.get("/:userId/all", IdeaController.getUserIdeas);
   router.post("/", IdeaController.create);
-  router.patch("/:ideaId", IdeaController.update);
-  router.delete("/:ideaId", IdeaController.delete);
-  router.post("/:ideaId/upvote", IdeaController.upvoteIdea);
-  router.post("/:ideaId/downvote", IdeaController.downvoteIdea);
+  router.patch("/:ideaId", AuthMiddleware, IdeaController.update);
+  router.delete("/:ideaId", AuthMiddleware, IdeaController.delete);
+  router.post("/:ideaId/upvote", AuthMiddleware, IdeaController.upvoteIdea);
+  router.post("/:ideaId/downvote", AuthMiddleware, IdeaController.downvoteIdea);
 
   return router;
 };
