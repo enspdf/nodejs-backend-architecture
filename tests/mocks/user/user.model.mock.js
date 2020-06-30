3d2eca832a7fa814930d4cf651f93be63a981b34
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-06-29 20:13:59 -0500

    ADD repository & service tests

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: "5ef97dc8aa64ac71e201c7bc",
+    name: "John Doe",
+    username: "john",
+    __v: 0,
+  },
+  users: [
+    {
+      _id: "5ef97dc8aa64ac71e201c7bc",
+      name: "John Doe",
+      username: "john",
+      __v: 0,
+    },
+    {
+      _id: "5efa0beff4726379d71fc141",
+      name: "john1 doe1",
+      username: "john1",
+      __v: 0,
+    },
+  ],
+};
